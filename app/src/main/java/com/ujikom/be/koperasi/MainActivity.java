71ef97d21ce4fc109a35db8d76eaf79040538f3e
Erfransyah Levi <franslevi008@gmail.com>
2017-02-28 12:40:18 +0700

    master, init

---
+++ app/src/main/java/com/ujikom/be/koperasi/MainActivity.java
@@ -0,0 +1,13 @@
+package com.ujikom.be.koperasi;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
